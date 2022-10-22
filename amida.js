81855f4df68b5d9ac37db0b4faaefe7ce63c7b9a
Takuya Azumaguchi <catchballer@gmail.com>
2022-10-22 16:22:07 +0900

    Get URL parameter num in Amida

--- amida.js
+++ amida.js
@@ -28,13 +28,37 @@ class VerticalLine {
       return Math.floor(Math.random() * 2);
   }
 
+  function getURLParameters() {
+    if (document.location.search.length === 0) {
+      console.log("URL paramer is zero");
+      return null;
+    }
+
+    var query = document.location.search.substring(1);
+    var parameters = query.split('&');
+
+    var result = new Object();
+    for (var i = 0; i < parameters.length; i++) {
+      var element = parameters[i].split('=');
+
+      var paramName = decodeURIComponent(element[0]);
+      var paramValue = decodeURIComponent(element[1]);
+
+      result[paramName] = decodeURIComponent(paramValue);
+    }
+    return result;
+  }
+
   function drawAmida() {
       const canvas = document.getElementById('canvas');
       if (!canvas || !canvas.getContext){
           return;
       }
 
-      const Tree = 7;
+      const parameters = getURLParameters();
+      const parameterNum = Object.keys(parameters).length;
+
+      const Tree = parameterNum;
       const TreeBlockCount = 15;
 
       const ctx = canvas.getContext('2d');
