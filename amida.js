c82d1046c6c92b520ab948e7f31b32b402e5d22e
Takuya Azumaguchi <catchballer@gmail.com>
2022-10-23 00:06:48 +0900

    Refactor js

--- amida.js
+++ amida.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const define = {};
 Object.defineProperty(define, 'canvasWidth', {
   value: 1200.0, writable: false
@@ -25,84 +27,84 @@ class VerticalLine {
   }
 }
 
+function getURLParameters() {
+  if (document.location.search.length === 0) {
+    console.log("URL paramer is zero");
+    return null;
+  }
+
+  var query = document.location.search.substring(1);
+  var parameters = query.split('&');
+
+  var result = new Object();
+  for (var i = 0; i < parameters.length; i++) {
+    var element = parameters[i].split('=');
+
+    var paramName = decodeURIComponent(element[0]);
+    var paramValue = decodeURIComponent(element[1]);
+
+    result[paramName] = decodeURIComponent(paramValue);
+  }
+  return result;
+}
+
+function addSourceNameList(nameNum, parameters) {
+    const fromNameListElement = document.getElementById('fromNameList');
+    if (fromNameListElement === null) {
+      return;
+    }
+
+    // add items that is source name
+    for (let i = 0; i < nameNum; i++) {
+      let button = document.createElement('button');
+      button.classList.add('item');
+      const keyName = 'name' + i;
+      button.textContent = parameters[keyName];
+      fromNameListElement.appendChild(button);
+    }
+}
+
+function addDistNameList(nameNum, parameters) {
+    const distNameListElement = document.getElementById('distNameList');
+    if (distNameListElement === null) {
+      return;
+    }
+
+    // add items that is dist name
+    for (let i = 0; i < nameNum; i++) {
+      let p = document.createElement('p');
+      p.classList.add('item');
+      const keyName = 'name' + i;
+      p.textContent = parameters[keyName];
+      distNameListElement.appendChild(p);
+    }
+}
+
+function drawAmida(nameNum) {
+    const canvas = document.getElementById('canvas');
+    if (!canvas || !canvas.getContext){
+        return;
+    }
+
+    // draw amida to canvas
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = 'black';
+
+    let verticalLines = [];
+    for (let i = 0; i < nameNum; i++) {
+      verticalLines[i] = new VerticalLine(i, define.treeBlockCount, nameNum);
+      verticalLines[i].draw(ctx, 1.0);
+    }
+}
+
 {
-  'use strict'
-  function random() {
-      return Math.floor(Math.random() * 2);
+  const parameters = getURLParameters();
+  const nameNum = Object.keys(parameters).length;
+  if (nameNum !== 0) {
+    addSourceNameList(nameNum, parameters);
+    addDistNameList(nameNum, parameters);
+
+    drawAmida(nameNum);
   }
-
-  function getURLParameters() {
-    if (document.location.search.length === 0) {
-      console.log("URL paramer is zero");
-      return null;
-    }
-
-    var query = document.location.search.substring(1);
-    var parameters = query.split('&');
-
-    var result = new Object();
-    for (var i = 0; i < parameters.length; i++) {
-      var element = parameters[i].split('=');
-
-      var paramName = decodeURIComponent(element[0]);
-      var paramValue = decodeURIComponent(element[1]);
-
-      result[paramName] = decodeURIComponent(paramValue);
-    }
-    return result;
-  }
-
-  function drawAmida() {
-      const canvas = document.getElementById('canvas');
-      if (!canvas || !canvas.getContext){
-          return;
-      }
-
-      const parameters = getURLParameters();
-      const nameNum = Object.keys(parameters).length;
-      if (nameNum === 0) {
-        return;
-      }
-
-      const fromNameListElement = document.getElementById('fromNameList');
-      if (fromNameListElement === null) {
-        return;
-      }
-
-      // add items that is source name
-      for (let i = 0; i < nameNum; i++) {
-        button = document.createElement('button');
-        button.classList.add('item');
-        const keyName = 'name' + i;
-        button.textContent = parameters[keyName];
-        fromNameListElement.appendChild(button);
-      }
-
-      // draw amida to canvas
-      const ctx = canvas.getContext('2d');
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.strokeStyle = 'black';
-
-      let verticalLines = [];
-      for (let i = 0; i < nameNum; i++) {
-        verticalLines[i] = new VerticalLine(i, define.treeBlockCount, nameNum);
-        verticalLines[i].draw(ctx, 1.0);
-      }
-
-      const distNameListElement = document.getElementById('distNameList');
-      if (distNameListElement === null) {
-        return;
-      }
-
-      // add items that is dist name
-      for (let i = 0; i < nameNum; i++) {
-        p = document.createElement('p');
-        p.classList.add('item');
-        const keyName = 'name' + i;
-        p.textContent = parameters[keyName];
-        distNameListElement.appendChild(p);
-      }
-  }
-
-  drawAmida();
 }
