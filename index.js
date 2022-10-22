d6008c8fbe80ec70f83fb0797822f35ed83e971b
Takuya Azumaguchi <catchballer@gmail.com>
2022-10-22 16:21:50 +0900

    Add index page

---
+++ index.js
@@ -0,0 +1,36 @@
+function addName() {
+  const nameTextElement = document.getElementById("nameText");
+  const nameText = nameTextElement.value;
+
+  if (nameText !== "") {
+    p = document.createElement('p');
+    p.appendChild(document.createTextNode(nameText));
+
+    const nameList = document.getElementById("nameList");
+    nameList.appendChild(p);
+
+    nameTextElement.value = '';
+    nameTextElement.focus();
+  }
+}
+
+function goAmida() {
+  const nameList = document.getElementById("nameList");
+  names = nameList.children;
+  if (names.length === 0) {
+    return;
+  }
+
+  path = 'amida.html?';
+  for (let i = 0; i < names.length; i++) {
+    console.log(names[i].textContent);
+    path += 'name' + i + '=' + names[i].textContent;
+
+    if (i !== names.length - 1) {
+      path += '&';
+    }
+  }
+  console.log(path);
+
+  window.location.href = path;
+}
